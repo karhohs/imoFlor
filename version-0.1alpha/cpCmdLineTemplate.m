44ddd65bec69f54ed654c3aa90e623fba167eccc
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2013-03-13 20:28:17 -0400

    update

--- version-0.1alpha/cpCmdLineTemplate.m
+++ version-0.1alpha/cpCmdLineTemplate.m
@@ -1,1 +1,25 @@
-%CellProfiler.exe -c -r -i ~/my_image_directory -o ~/my_output_directory -p ~/my_pipe.mat
+%% CellProfiler call from the DOS command line Template
+% When using CellProfiler to analyze several different sets of images it is
+% convenient to repeatedly call CellProfiler from the command line. MATLAB
+% can run a command from DOS (or UNIX in a Mac). This script is template.
+% Just add the appropriate paths to the variables below. Make sure each
+% variable has the same number of elements, so if a pipeline is used
+% mulitple times, it should appear in the _pipelinepath_ variable multiple
+% times.
+%
+% * pipelinepath: the path to each CellProfiler pipeline variable
+% * inpath: the path to the folder that contains the CSV file
+% * outpath: self-explanatory
+pipelinepath = {'',...
+    ''};
+inpath = {'',...
+    ''};
+outpath = {'',...
+    ''};
+
+%%
+%
+for i=1:length(pipelinepath)
+str = sprintf('CellProfiler.exe -c -r -i %s -o %s -p %s',inpath{i},outpath{i},pipelinepath{i});
+dos(str);
+end
