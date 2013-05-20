494df2b40d15a9506b81d075e3167ba0ab5ee574
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2013-05-20 00:49:00 -0400

    updated markup in m files

--- version-0.1alpha/visualsCookbook/cpGrowthCurve.m
+++ version-0.1alpha/visualsCookbook/cpGrowthCurve.m
@@ -1,4 +1,21 @@
+%% cpGrowthCurve
+%
+%% Inputs
+% * |data|, a required variable. |data| is a cell array containing time
+% series data. In each position of the cell array resides a column vector.
+% There is a row for each image in the column vector and the number in each
+% row represents the number of cells segmented in that image.
+% * |outpath|, the directory where image files will be saved. By default
+% this is the user's home in the MATLAB(R) path.
+%%% Outputs
+% * |figh|, a struct containing the figure handles for each plot created by
+% this function. This output is useful for tweaking these plots after the
+% function call.
 function [figh] = cpGrowthCurve(data,varargin)
+%% Parse input
+% The inputs into the function are parsed. If there were no inputs when the
+% function was called a set of demonstrative data is imported and
+% processed; this is useful and necessary for MATLAB-publishing this file.
 if nargin == 0
     data = loadDemoData('cpGrowthCurve');
 end
@@ -17,7 +34,6 @@ end
 if ~isdir(outpath)
     mkdir(outpath);
 end
-
 %% boxplot
 numImages = cellfun(@length,data);
 maxNumImages = max(numImages);
@@ -32,7 +48,11 @@ ylabel(str);
 set(figh.boxplot,'PaperSize',[7 4]);
 set(figh.boxplot,'PaperUnits','inches','PaperPosition',[0 0 6.4 3.6]);
 rez=150; %resolution (dpi) of final graphic
-print(figh.boxplot,fullfile(outpath,'cpGrowthCurve_boxplot.png'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+print(figh.boxplot,fullfile(outpath,'cpGrowthCurve_boxplot.png'),'-dpng',['-r',num2str(rez)],'-opengl'); %save file 
 print(figh.boxplot,fullfile(outpath,'cpGrowthCurve_boxplot.pdf'),'-dpdf');
 %%
 meancelldensity = cellfun(@mean,data)./cellfun(@length,data);
+%% Full Size Demo Figures
+%
+% <<cpGrowthCurve_boxplot.png>>
+%
--- version-0.1alpha/visualsCookbook/cpHistogram.m
+++ version-0.1alpha/visualsCookbook/cpHistogram.m
@@ -1,4 +1,24 @@
+%% cpHistogram
+%
+%% Inputs
+% * |data|, a required variable. |data| is a cell array containing time
+% series data. In each position of the cell array resides a column vector.
+% There is a row for every cell measurement in the column vector.
+% * |outpath|, the directory where image files will be saved. By default
+% this is the user's home in the MATLAB(R) path.
+% * |nbins|, the number of bins within a histogram or if this is an array,
+% the centers of histogram boxes. The default value is 100.
+% * |titles|, the titles that will be placed above each figure. There must
+% be a title for each column vector in the |data| cell array.
+%%% Outputs
+% * |figh|, a struct containing the figure handles for each plot created by
+% this function. This output is useful for tweaking these plots after the
+% function call.
 function [figh] = cpHistogram(data,varargin)
+%% Parse input
+% The inputs into the function are parsed. If there were no inputs when the
+% function was called a set of demonstrative data is imported and
+% processed; this is useful and necessary for MATLAB-publishing this file.
 if nargin == 0
     data = loadDemoData('cpHistogram');
 end
@@ -40,3 +60,9 @@ for i=1:length(data)
     str = sprintf('cpHistogram_%s.pdf',p.Results.titles{i});
     print(figh(i).hist,fullfile(outpath,str),'-dpdf');
 end
+%% Full Size Demo Figures
+%
+% <<cpHistogram_1.png>>
+%
+% <<cpHistogram_2.png>>
+%
